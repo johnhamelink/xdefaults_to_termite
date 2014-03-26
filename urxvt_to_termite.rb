7642af6fd18faf91205b256e0e206a8f04a2b26d
John Hamelink <hello@farmer.io>
2014-03-26 02:43:49 +0000

    Added colours flag so that the script will generate concatenatable output

--- urxvt_to_termite.rb
+++ urxvt_to_termite.rb
@@ -10,6 +10,8 @@ require_relative 'line_scanner'
 require_relative 'urxvt_reader'
 
 reader = UrxvtReader.new(ARGV.first)
+puts "# This theme was imported with urxvt_to_termite.rb"
+puts "[colors]"
 reader.options.each do |opt|
   puts "#{opt.key} = #{opt.value}"
 end
